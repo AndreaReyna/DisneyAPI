ae68bd9f22de9d2374c58fc95fafec61fa1fc344
Andrea Reyna <andree.reyna@gmail.com>
2022-02-01 11:36:54 -0300

    SecurityConstants agregado

---
+++ src/main/java/com/AlkemyChallange/Disney/security/SecurityConstants.java
@@ -0,0 +1,11 @@
+package com.AlkemyChallange.Disney.security;
+
+public class SecurityConstants {
+
+    public static final String SECRET = "SECRET_KEY";
+    public static final long EXPIRATION_TIME = 900_000;
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/auth/login";
+    public static final String SENDGRID_API_KEY = "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";
+}
